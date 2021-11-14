6d0969cd83b4e016731030f037d3ee5aaaf25fd6
Ethan <ethanstm8@gmail.com>
2021-11-14 17:02:13 -0500

    Node declarations

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Structures
+
+A C++ implementation of various data structures. I abandoned the STL project in favor of just implementing data structures. I'll possibly merge these into my STL project some day, and continue with it. 
--- avl_tree.hpp
+++ avl_tree.hpp
@@ -1,10 +1,26 @@
 
+#include <optional>
+
 template<typename T> struct Node {
 	T elem;
 	T* parent = nullptr;
 	T* son, daughter = nullptr;
+
+	void place(T* loc);
+	inline bool hasParent() { return parent != nullptr; }
+	inline bool hasSon() { return son != nullptr; }
+	inline bool hasDaughter() { return daughter != nullptr; }
+	inline bool hasChild() { return hasSon() || hasDaughter(); }
+
+	inline bool isSon() { return hasParent() && this == parent->son; }
+	inline bool isDaughter() { return hasParent() && this == parent->daughter; }
+
+	Node(T _elem) : elem{_elem} {};
 }
 
 template<typename T> class AvlTree {
 	T* root;
+
+public:
+	void insert(T elem);
 }
